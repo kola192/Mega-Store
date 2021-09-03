ccedde289e33b3f73192908f24bb6df8236f6253
mohap710 <mohap710@gmail.com>
2021-09-03 17:34:47 +0200

    Added filters.js file with the TotalSum Filter

---
+++ js/filters.js
@@ -0,0 +1,13 @@
+angular.module('megaStore')
+.filter('totalSumPriceQty', function() {
+        return function (data, key1, key2) {
+            if (angular.isUndefined(data) || angular.isUndefined(key1) || angular.isUndefined(key2))
+                return 0;
+
+            var sum = 0;
+            angular.forEach(data, function (v, k) {
+                sum = sum + (parseInt(v[key1]) * parseInt(v[key2]));
+            });
+            return sum;
+        }
+    })
